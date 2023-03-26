43e23bd68f2b38d32e131b555513fb7e7cbfc0b7
halynahnatiuk <124817652+halynahnatiuk@users.noreply.github.com>
2023-03-26 23:50:18 +0300

    Completed challenge 2

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,45 @@
 package com.serenitydojo.calculator;
 
+import com.google.common.base.Splitter;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class Calculator {
-    public int evaluate(String expression) {
-        return 0;
+
+    public int evaluate(String expression)  {
+
+        if (expression.isEmpty()) {
+            return 0;
+        }
+        List<String> tokens = Splitter.on(" ").splitToList(expression);
+
+        int totalAmount = 0;
+        String nextOperator = "+";
+
+        for (String token:tokens){
+            if (!isNumeric(token)){
+                nextOperator = token;
+            }
+            else totalAmount= process(totalAmount, nextOperator, token);
+        }
+        return totalAmount;
+
+    }
+
+    private int process(int totalAmount, String nextOperator, String token) {
+        switch (nextOperator){
+            case "+":
+                return totalAmount + Integer.parseInt(token);
+            case "-":
+                return totalAmount - Integer.parseInt(token);
+            case"*":
+                return totalAmount * Integer.parseInt(token);
+            default:
+                throw new IllegalMathsOperatorException("Unsupported operator " + nextOperator);
+        }
+
     }
 }
